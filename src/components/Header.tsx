e26a04dd557d26a80fda153e6f0eacea2a0fb185
Kliv <noreply@kliv.dev>
2025-11-19 19:47:51 +0000

    Import React in Header component for proper usage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import { LocalizedLink } from './LocalizedLink';
